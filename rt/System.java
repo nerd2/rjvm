f0275abb2c0af543c72d1d6de6f89101026f86b3
Sam <sam@gpsm.co.uk>
2017-07-11 16:07:19 +0100

    System getSecurityManager

--- rt/System.java
+++ rt/System.java
@@ -1,6 +1,8 @@
 package java.lang;
 
 class System {
+    private static SecurityManager security = new SecurityManager();
+
     public static void arraycopy(Object src, int srcPos, Object dest, int destPos, int len) {
         Object[] srcArray = (Object[])src;
         Object[] destArray = (Object[])dest;
@@ -10,5 +12,9 @@ class System {
             destPos++;
             len--;
         }
+    }
+
+    public static SecurityManager getSecurityManager() {
+        return security;
     }
 }
